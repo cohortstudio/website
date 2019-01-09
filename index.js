fcf8a0481f8919ae9c491d9e0b203467424123a3
Vincent Battaglia <vincent.battaglia@gmail.com>
2019-01-09 14:23:00 +0100

    Rewrite with Express

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Home'))
+app.get('/contact', (req, res) => res.send('Contact'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
