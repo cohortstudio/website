d22c577f62f94b7561034294db34e9dc55c39017
Vincent Battaglia <vincent.battaglia@gmail.com>
2019-01-09 14:33:18 +0100

    Changed port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = (process.env.PORT || 8050);
 
 app.get('/', (req, res) => res.send('Home'))
 app.get('/contact', (req, res) => res.send('Contact'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
