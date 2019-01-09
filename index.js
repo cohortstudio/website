c1882c87624e60de439e89584082d446c914364c
Vincent Battaglia <vincent.battaglia@gmail.com>
2019-01-09 16:35:32 +0100

    Redirect www to non-www

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = (process.env.PORT || 8050);
 
-app.get('/', (req, res) => res.send('Home'))
-app.get('/contact', (req, res) => res.send('Contact'))
+app.all('/*', (req, res, next) => {
+  if (req.headers.host.slice(0, 4) === 'www.') {
+    var newHost = req.headers.host.slice(4);
+    return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+  } else {
+    next();     
+  }
+});
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.get('/', (req, res) => res.send('Home'));
+app.get('/contact', (req, res) => res.send('Contact'));
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
