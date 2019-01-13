c5183c4eb80371d7b8f962a75fa2d03215c0bb28
Vincent Battaglia <vincent.battaglia@gmail.com>
2019-01-13 19:00:34 +0100

    New design

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const path = require('path');
+const appRootPath = require('app-root-path');
+
 const app = express();
+
 const PORT = (process.env.PORT || 8050);
+const APP_ROOT_PATH = appRootPath.toString();
+
+app.set('view engine', 'pug');
+
+app.use('/assets', express.static(path.join(APP_ROOT_PATH, 'assets')));
 
 app.all('/*', (req, res, next) => {
   if (req.headers.host.slice(0, 4) === 'www.') {
@@ -11,6 +20,6 @@ app.all('/*', (req, res, next) => {
   }
 });
 
-app.get('/', (req, res) => res.send('Home'));
+app.get('/', (req, res) => res.render('home'));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
